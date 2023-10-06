0304ac72fad1c1debebc5d9f3565b9598bfd6bca
raidas <mrraidas@gmail.com>
2023-10-06 12:02:31 +0300

    more examples to experiments dir

---
+++ src/experiments/fieldSize.ts
@@ -0,0 +1,22 @@
+/*
+Provable comparison functions can only be used on Fields of size <= 253 bits, got 255 bits.
+*/
+
+import { Field, method, Experimental, Provable } from 'o1js';
+
+const myProgram = Experimental.ZkProgram({
+  methods: {
+    prove: {
+      privateInputs: [Field, Field, Field],
+      method(number1: Field, number2: Field, number3: Field) {
+        number1.sub(number2).greaterThan(number3);
+      },
+    },
+  },
+});
+
+// create proofs
+await myProgram.compile();
+
+// throws the error
+const proof = await myProgram.prove(Field(10), Field(9), Field(5));
