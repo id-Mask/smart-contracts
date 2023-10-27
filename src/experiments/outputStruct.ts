21fb45a37a5cecec6b73fa45733b34f37e6dec50
raidas <mrraidas@gmail.com>
2023-10-27 18:00:22 +0300

    Struct output example

---
+++ src/experiments/outputStruct.ts
@@ -0,0 +1,55 @@
+/*
+I want to create a SmartContract method, that consume and verify specified proof.
+
+Reading over other examples, I understood that this is possible by specifying the proof class in the method args
+(this bakes in the program's verification key inside the smart contract's verification key), i.e.
+class MyProgram extends Experimental.ZkProgram.Proof(myProgram) {}
+@method verifyProof(proof: MyProgram) { proof.verify() }
+
+But it does not seem to work in the example below. What am I doing wrong?
+
+https://discord.com/channels/484437221055922177/1125872479030747136/1125875896302194708
+https://discord.com/channels/484437221055922177/1080552939313184859/1080598740890562691
+*/
+
+import {
+  Field,
+  Bool,
+  method,
+  Experimental,
+  SmartContract,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  Provable,
+  Mina,
+  Struct,
+  CircuitString,
+} from 'o1js';
+
+class zkProofOutput extends Struct({
+  proofValid: Bool,
+  currentDate: CircuitString,
+}) {}
+
+const myProgram = Experimental.ZkProgram({
+  publicOutput: zkProofOutput,
+  methods: {
+    prove: {
+      privateInputs: [Field],
+      method(value: Field): typeof publicOutput {
+        value.assertEquals(Field(1));
+        const publicOutput = new zkProofOutput({
+          proofValid: Bool(true),
+          currentDate: CircuitString.fromString('2025-01-01'),
+        });
+        return publicOutput;
+      },
+    },
+  },
+});
+
+// run
+await myProgram.compile();
+const proof = await myProgram.prove(Field(1));
+console.log(JSON.stringify(proof.toJSON()).substring(0, 300));
