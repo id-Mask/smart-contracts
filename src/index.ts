f16dcf89ba9695d4dc50c0c807848a37a7720ba3
raidas <mrraidas@gmail.com>
2023-12-04 18:12:39 +0200

    add exports to be imported from the npm package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { ProofOfAge } from './ProofOfAge.js';
+// export so that its possible to import it using the npm package
 
-export { ProofOfAge };
+import { proofOfAge, ProofOfAge, ProofOfAgeProof } from './ProofOfAge.js';
+import { PersonalData } from './ProofOfAge.utils.js';
+
+import {
+  proofOfSanctions,
+  ProofOfSanctions,
+  ProofOfSanctionsProof,
+} from './ProofOfSanctions.js';
+
+export {
+  proofOfAge,
+  ProofOfAge,
+  ProofOfAgeProof,
+  PersonalData,
+  proofOfSanctions,
+  ProofOfSanctions,
+  ProofOfSanctionsProof,
+};
