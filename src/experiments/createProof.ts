541a8a2916ded57e8841e15544a7bb4c183543f3
raidas <mrraidas@gmail.com>
2024-03-12 17:49:36 +0200

    update experiments

---
+++ src/experiments/createProof.ts
@@ -0,0 +1,22 @@
+import { Field, ZkProgram, Cache } from 'o1js';
+import fs from 'fs';
+
+export const myProgram = ZkProgram({
+  name: 'myProgram',
+  publicOutput: Field,
+  methods: {
+    prove: {
+      privateInputs: [Field],
+      method(value: Field): Field {
+        value.assertEquals(Field(1));
+        return Field(1);
+      },
+    },
+  },
+});
+
+// create proof
+const cache: Cache = Cache.FileSystem('./cache');
+await myProgram.compile({ cache });
+const proof = await myProgram.prove(Field(1));
+await fs.promises.writeFile('proof.json', JSON.stringify(proof.toJSON()));
